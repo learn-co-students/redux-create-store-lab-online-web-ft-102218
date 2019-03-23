d587e494fa06f61a3128609bb4d8ddc8c447470a
kodyclemens <kody.clemens@gmail.com>
2019-03-23 18:16:08 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,19 @@
+import candyReducer from './reducers/candyReducer';
+import countReducer from './reducers/countReducer';
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function getState() {
+    return reducer(state, {type: '@@INIT'});
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  return { dispatch, getState }
 }
 
 function render() {
